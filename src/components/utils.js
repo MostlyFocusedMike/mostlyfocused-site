173b5fca39ce8d209db043a242dde06a7b37c1d5
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-07-30 09:56:22 -0400

    Publish lazy article

--- src/components/utils.js
+++ src/components/utils.js
@@ -10,13 +10,13 @@ export const escapeText = (str) => str.replace(/&/g,"&amp;").replace(/</g,"&lt;"
 export const $m = (array, callback) => array.map(callback).join('');
 
 /**
- * utility wrapper for JSON.stringify, with error handling
+ * utility wrapper for JSON.stringify with base64, with error handling
  * @param {any} value val to stringify
  * @returns stringified object | ''
  */
 export const $s = (value) => {
   try {
-    return JSON.stringify(value);
+    return btoa(JSON.stringify(value));
   } catch (err) {
     console.error(err)
     return '';
@@ -24,13 +24,13 @@ export const $s = (value) => {
 };
 
 /**
- * utility wrapper for JSON.parse, with error handling
+ * utility wrapper for JSON.parse and base64, with error handling
  * @param {any} value val to parse
  * @returns parsed value | false
  */
 export const $p = (value) => {
   try {
-    return JSON.parse(value);
+    return JSON.parse(atob(value));
   } catch (err) {
     console.error(err)
     return false;
