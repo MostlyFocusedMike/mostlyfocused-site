89482a9d4f9facb380295c82511549d10c0f3ba8
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-09-22 00:57:29 -0400

    send over the right data from traffic analyzer

--- src/components/traffic-analyzer/index.js
+++ src/components/traffic-analyzer/index.js
@@ -1,8 +1,21 @@
 export default class TrafficAnalyzer extends HTMLElement {
   connectedCallback() {
-    fetch('https://traffic.mostlyfocused.com/traffic-analyzer')
-      .then(r => r.text())
-      .then(console.log)
-      .catch(console.error)
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const referrer = urlParams.get('custom-refer') || document.referrer;
+
+    const data = { referrer, route: window.location.pathname };
+
+    const query = Object.fromEntries(urlParams.entries());
+    if (Object.keys(query).length) data.query = query;
+
+    if (window.location.hash) data.hash = window.location.hash;
+
+    const opts = { method: 'POST', body: JSON.stringify(data) };
+    const path = import.meta.env.DEV
+      ? 'http://localhost:3000/traffic-analyzer'
+      : 'https://traffic.mostlyfocused.com/traffic-analyzer';
+
+    setTimeout(() => fetch(path, opts).catch(console.error), 3000);
   }
 }
