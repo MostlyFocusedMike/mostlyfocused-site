8438d571b9ff820d782357e1496df8b0b732013d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-09-22 01:16:37 -0400

    add timestamp and proper headers

--- src/components/traffic-analyzer/index.js
+++ src/components/traffic-analyzer/index.js
@@ -1,21 +1,32 @@
 export default class TrafficAnalyzer extends HTMLElement {
   connectedCallback() {
     const urlParams = new URLSearchParams(window.location.search);
+    const now = new Date();
+    const timestamp = now.toUTCString();
+
+    const data = { timestamp, route: window.location.pathname, };
 
     const referrer = urlParams.get('custom-refer') || document.referrer;
-
-    const data = { referrer, route: window.location.pathname };
+    if (referrer) data.referrer = referrer;
 
     const query = Object.fromEntries(urlParams.entries());
     if (Object.keys(query).length) data.query = query;
 
     if (window.location.hash) data.hash = window.location.hash;
 
-    const opts = { method: 'POST', body: JSON.stringify(data) };
+    const opts = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    };
+    const timeout = import.meta.env.DEV ? 0 : 3000;
     const path = import.meta.env.DEV
       ? 'http://localhost:3000/traffic-analyzer'
       : 'https://traffic.mostlyfocused.com/traffic-analyzer';
 
-    setTimeout(() => fetch(path, opts).catch(console.error), 3000);
+    setTimeout(() => fetch(path, opts).catch(console.error), timeout);
   }
 }
