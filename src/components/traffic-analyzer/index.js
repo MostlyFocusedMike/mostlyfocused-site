2b65c0fc55b5bcd26cb112b21f5357536003787f
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-09-24 23:14:15 -0400

    drop route hash and query params from payload

--- src/components/traffic-analyzer/index.js
+++ src/components/traffic-analyzer/index.js
@@ -2,18 +2,13 @@ const headers = { 'Accept': 'application/json', 'Content-Type': 'application/jso
 
 export default class TrafficAnalyzer extends HTMLElement {
   connectedCallback() {
-    const urlParams = new URLSearchParams(window.location.search);
     const timestamp = (new Date()).toISOString();
 
     const data = { timestamp, route: window.location.pathname, };
 
+    const urlParams = new URLSearchParams(window.location.search);
     const referrer = urlParams.get('custom-refer') || document.referrer;
     if (referrer) data.referrer = referrer;
-
-    const queryParams = Object.fromEntries(urlParams.entries());
-    if (Object.keys(queryParams).length) data.queryParams = queryParams;
-
-    if (window.location.hash) data.routeHash = window.location.hash;
 
     const opts = { method: 'POST', headers, body: JSON.stringify(data) };
 
